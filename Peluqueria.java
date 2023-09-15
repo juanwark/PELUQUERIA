954b394a222984f5ad47b9f9e5111ca31f5f84a5
juanwark <142903745+juanwark@users.noreply.github.com>
2023-09-15 17:49:40 -0500

    Add files via upload

---
+++ Peluqueria.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peluqueria;
+
+/**
+ *
+ * @author Juan David
+ */
+public class Peluqueria {
+
+    public static void main(String[] args) {
+         interfaz in = new interfaz();
+        in.setVisible(true);
+        
+    }
+}
